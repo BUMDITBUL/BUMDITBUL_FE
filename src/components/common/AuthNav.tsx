a74f5e519c7981ad4fbb718b436f8aee83baca6a
Doeun <qwaszx080402@naver.com>
2026-04-07 15:12:59 +0900

    fix: AuthNav 로그인 버튼 색상 수정

--- src/components/common/AuthNav.tsx
+++ src/components/common/AuthNav.tsx
@@ -2,7 +2,7 @@ export default function AuthNav() {
   return (
     <nav className="flex items-center justify-between px-10 py-3 shrink-0 border-b border-white/10">
       <img src="/images/logo.svg" alt="범딧불" className="w-8 h-8" />
-      <a href="/login" className="underline text-brand-green-600" style={{ fontSize: "15px", fontWeight: 500 }}>
+      <a href="/login" className="underline text-brand-green-400" style={{ fontSize: "15px", fontWeight: 500 }}>
         로그인
       </a>
     </nav>
