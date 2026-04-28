b767a89c0482f9aa80c8f9eb8eea25faf0c9bebd
Doeun <qwaszx080402@naver.com>
2026-04-28 11:51:23 +0900

    feat: 시험 당일 D-DAY 응원 팝업 추가 (ExamDayModal)

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,10 +2,12 @@ import MainNav from "@/components/common/MainNav";
 import HeroBanner from "@/features/main/components/HeroBanner";
 import MainCalendar from "@/features/main/components/MainCalendar";
 import StudyPanel from "@/features/main/components/StudyPanel";
+import ExamDayModal from "@/components/ui/ExamDayModal";
 
 export default function Home() {
   return (
     <div className="h-screen flex flex-col bg-brand-black-900 overflow-hidden">
+      <ExamDayModal />
       <MainNav />
       <div className="flex-1 min-h-0 flex justify-center overflow-hidden">
         <div className="w-full max-w-[1080px] flex flex-col min-h-0 pb-5 gap-5 px-6">
